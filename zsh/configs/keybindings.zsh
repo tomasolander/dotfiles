8e2ae27091b5aae4ceb3a8434ab9420e8bfcc80f
Tomas Olander <tomasolander@gmail.com>
2016-04-02 09:27:17 -0400

    Enable fuzzy find history with up/down arrows

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -11,3 +11,11 @@ bindkey "^P" history-search-backward
 bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+# fuzzy find history when typing has started
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
