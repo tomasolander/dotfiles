a3b7adf959a23186f4b441d10ff7f0123052565e
Tomas Olander <tomasolander@gmail.com>
2016-04-02 09:26:03 -0400

    Tweak zsh completion

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -4,3 +4,12 @@ fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 # completion
 autoload -U compinit
 compinit
+
+zstyle ':completion:*' matcher-list \
+  'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order \
+  local-directories \
+  directory-stack \
+  path-directories
