38fb8c2365a4af0884860fa94a6d6b3156f597c5
Tomas Olander <tomasolander@gmail.com>
2019-11-13 14:30:07 +0100

    Fix word stepping on OSX

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -11,8 +11,8 @@ bindkey "^P" history-search-backward
 bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
-bindkey "^[[D" backward-word
-bindkey "^[[C" forward-word
+bindkey "^[[1;3D" backward-word
+bindkey "^[[1;3C" forward-word
 
 # fuzzy find history when typing has started
 autoload -U up-line-or-beginning-search
